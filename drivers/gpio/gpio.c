7bffee1c0bab95b2dfa68784bdda41ee917a6161
Toby <dongocthach12@gmail.com>
2026-05-10 23:47:17 +0700

    Update driver barematel for GPIO module

--- drivers/gpio/gpio.c
+++ drivers/gpio/gpio.c
@@ -1,31 +1,150 @@
-#include <stdint.h>
+/**
+ * @file    gpio.c
+ * @brief   GPIO driver for STM32F411 (Baremetal)
+ * @author  Toby
+ */
 #include "gpio.h"
 
-/* RCC */
-#define RCC_BASE        0x40023800
-#define RCC_AHB1ENR     (*(volatile uint32_t*)(RCC_BASE + 0x30))
-
-/* GPIOA */
-#define GPIOA_BASE      0x40020000
-#define GPIOA_MODER     (*(volatile uint32_t*)(GPIOA_BASE + 0x00))
-#define GPIOA_ODR       (*(volatile uint32_t*)(GPIOA_BASE + 0x14))
-
-/* GPIOC */
-#define GPIOC_BASE      0x40020800
-#define GPIOC_MODER     (*(volatile uint32_t*)(GPIOC_BASE + 0x00))
-#define GPIOC_ODR       (*(volatile uint32_t*)(GPIOC_BASE + 0x14))
-
-void gpio_init(void)
+/**
+ * @brief  Enable clock for GPIOX of STM32F411 Blackpill
+ * @param  gpio: GPIO base address
+ * @retval None
+ */
+void gpio_clock_enable(gpio_t *gpio)
 {
-   /* Enable GPIOC clock */
-    RCC_AHB1ENR |= (1 << 2);
+    if (gpio == GPIOA)
+        RCC_AHB1ENR |= (1 << 0);
 
-    /* PC13 = output */
-    GPIOC_MODER &= ~(3 << (13 * 2));
-    GPIOC_MODER |=  (1 << (13 * 2));
+    else if (gpio == GPIOB)
+        RCC_AHB1ENR |= (1 << 1);
+
+    else if (gpio == GPIOC)
+        RCC_AHB1ENR |= (1 << 2);
+
+    else if (gpio == GPIOD)
+        RCC_AHB1ENR |= (1 << 3);
+
+    else if (gpio == GPIOE)
+        RCC_AHB1ENR |= (1 << 4);
 }
 
-void gpio_toggle(void)
+/**
+ * @brief  Setting GPIO Mode of STM32F411 Blackpill
+ * @param  gpio: GPIO base address
+ * @param  pin:  Pin value
+ * @param  mode: Mode of pin
+ * @retval None
+ */
+void gpio_mode(gpio_t *gpio, uint8_t pin, gpio_mode_t mode)
 {
-    GPIOC_ODR ^= (1 << 13);
+    gpio->MODER &= ~(0x3 << (pin * 2));
+
+    gpio->MODER |= (mode << (pin * 2));
+}
+
+/**
+ * @brief  Setting GPIO Output type of STM32F411 Blackpill
+ * @param  gpio: GPIO base address
+ * @param  pin:  Pin value
+ * @param  type: Output type pin
+ * @retval None
+ */
+void gpio_output_type(gpio_t *gpio, uint8_t pin, gpio_otype_t type)
+{
+    gpio->OTYPER &= ~(1 << pin);
+
+    gpio->OTYPER |= (type << pin);
+}
+
+/**
+ * @brief  Setting GPIO Pull Up/ Pull Down of STM32F411 Blackpill
+ * @param  gpio: GPIO base address
+ * @param  pin:  Pin value
+ * @param  pull: pullup / pulldown
+ * @retval None
+ */
+void gpio_pull(gpio_t *gpio, uint8_t pin, gpio_pull_t pull)
+{
+    gpio->PUPDR &= ~(0x3 << (pin * 2));
+
+    gpio->PUPDR |= (pull << (pin * 2));
+}
+
+/**
+ * @brief  Setting GPIO speed of STM32F411 Blackpill
+ * @param  gpio: GPIO base address
+ * @param  pin:  Pin value
+ * @param  speed: Speed
+ * @retval None
+ */
+void gpio_speed(gpio_t *gpio, uint8_t pin, gpio_speed_t speed)
+{
+    gpio->OSPEEDR &= ~(0x3 << (pin * 2));
+
+    gpio->OSPEEDR |= (speed << (pin * 2));
+}
+
+/**
+ * @brief  Setting GPIO alternate functon of STM32F411 Blackpill
+ * @param  gpio: GPIO base address
+ * @param  pin:  Pin value
+ * @param  af: alternate functon
+ * @retval None
+ */
+void gpio_af(gpio_t *gpio, uint8_t pin, uint8_t af)
+{
+    if (pin < 8)
+    {
+        gpio->AFRL &= ~(0xF << (pin * 4));
+
+        gpio->AFRL |= (af << (pin * 4));
+    }
+    else
+    {
+        pin -= 8;
+
+        gpio->AFRH &= ~(0xF << (pin * 4));
+
+        gpio->AFRH |= (af << (pin * 4));
+    }
+}
+
+/**
+ * @brief  Setting GPIO write pin of STM32F411 Blackpill
+ * @param  gpio: GPIO base address
+ * @param  pin:  Pin value
+ * @param  state: state
+ * @retval None
+ */
+void gpio_write(gpio_t *gpio, uint8_t pin, uint8_t state)
+{
+    if (state)
+        gpio->BSRR = (1 << pin);
+
+    else
+        gpio->BSRR = (1 << (pin + 16));
+}
+
+/**
+ * @brief  Setting GPIO toggle function of STM32F411 Blackpill
+ * @param  gpio: GPIO base address
+ * @param  pin:  Pin value
+ * @retval None
+ */
+void gpio_toggle(gpio_t *gpio,
+                 uint8_t pin)
+{
+    gpio->ODR ^= (1 << pin);
+}
+
+/**
+ * @brief  Setting GPIO read pin of STM32F411 Blackpill
+ * @param  gpio: GPIO base address
+ * @param  pin:  Pin value
+ * @returns None
+ */
+uint8_t gpio_read(gpio_t *gpio,
+                  uint8_t pin)
+{
+    return (gpio->IDR >> pin) & 0x1;
 }
--- drivers/gpio/gpio.h
+++ drivers/gpio/gpio.h
@@ -1,7 +1,103 @@
+/**
+ * @file    gpio.h
+ * @brief   GPIO driver for STM32F411 (Baremetal)
+ * @author  Toby
+ */
+
 #ifndef GPIO_H
 #define GPIO_H
 
-void gpio_init(void);
-void gpio_toggle(void);
+#include <stdint.h>
+
+/* GPIO Base Addresses */
+
+#define GPIOA_BASE 0x40020000
+#define GPIOB_BASE 0x40020400
+#define GPIOC_BASE 0x40020800
+#define GPIOD_BASE 0x40020C00
+#define GPIOE_BASE 0x40021000
+
+/* RCC Config */
+
+#define RCC_BASE        0x40023800
+#define RCC_AHB1ENR     (*(volatile uint32_t*)(RCC_BASE + 0x30))
+
+/* GPIO Struct */
+
+typedef struct
+{
+    volatile uint32_t MODER;
+    volatile uint32_t OTYPER;
+    volatile uint32_t OSPEEDR;
+    volatile uint32_t PUPDR;
+    volatile uint32_t IDR;
+    volatile uint32_t ODR;
+    volatile uint32_t BSRR;
+    volatile uint32_t LCKR;
+    volatile uint32_t AFRL;
+    volatile uint32_t AFRH;
+
+} gpio_t;
+
+/* GPIO Instances */
+
+#define GPIOA ((gpio_t*)GPIOA_BASE)
+#define GPIOB ((gpio_t*)GPIOB_BASE)
+#define GPIOC ((gpio_t*)GPIOC_BASE)
+#define GPIOD ((gpio_t*)GPIOD_BASE)
+#define GPIOE ((gpio_t*)GPIOE_BASE)
+
+/* GPIO Mode*/
+
+typedef enum
+{
+    GPIO_INPUT  = 0x0,
+    GPIO_OUTPUT = 0x1,
+    GPIO_AF     = 0x2,
+    GPIO_ANALOG = 0x3
+
+} gpio_mode_t;
+
+/* GPIO Output Type */
+
+typedef enum
+{
+    GPIO_PUSH_PULL  = 0,
+    GPIO_OPEN_DRAIN = 1
+
+} gpio_otype_t;
+
+/* GPIO Pull*/
+
+typedef enum
+{
+    GPIO_NO_PULL = 0,
+    GPIO_PULL_UP = 1,
+    GPIO_PULL_DOWN = 2
+
+} gpio_pull_t;
+
+/* GPIO Speed*/
+
+typedef enum
+{
+    GPIO_LOW_SPEED       = 0,
+    GPIO_MEDIUM_SPEED    = 1,
+    GPIO_FAST_SPEED      = 2,
+    GPIO_HIGH_SPEED      = 3
+
+} gpio_speed_t;
+
+/* Function Prototypes */
+
+void gpio_clock_enable(gpio_t *gpio);
+void gpio_mode(gpio_t *gpio, uint8_t pin, gpio_mode_t mode);
+void gpio_output_type(gpio_t *gpio, uint8_t pin, gpio_otype_t type);
+void gpio_pull(gpio_t *gpio, uint8_t pin, gpio_pull_t pull);
+void gpio_speed(gpio_t *gpio, uint8_t pin, gpio_speed_t speed);
+void gpio_af(gpio_t *gpio, uint8_t pin, uint8_t af);
+void gpio_write(gpio_t *gpio, uint8_t pin, uint8_t state);
+void gpio_toggle(gpio_t *gpio, uint8_t pin);
+uint8_t gpio_read(gpio_t *gpio, uint8_t pin);
 
 #endif
